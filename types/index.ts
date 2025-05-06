c90c80ec734b5d9ba79345bab10324fe67b5de99
itsAKDwivedi <akdvar2003@gmail.com>
2025-05-07 00:32:38 +0530

    Remove a invalid import from index.ts of types

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,5 @@
 import { ProductSchema } from "@/lib/validator";
 import {z} from "zod";
-import { process } from './../lib/db/index';
 
 export type IProductInput = z.infer<typeof ProductSchema>;
 
